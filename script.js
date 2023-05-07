b507559aa562da39c9b40692cea1e5b4f9c56694
Maksim Bekis <maxamax997@gmail.com>
2023-05-08 01:49:16 +0300

    slider-mobi-edit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let img = document.querySelectorAll(".sliderLine img");
-let sliderLine = document.querySelector(".sliderLine");
-let tchk = document.querySelectorAll(".all__tchk .tchk");
+let sliderLine = document.querySelector(".sliderLine-mobi");
+let dots = document.querySelectorAll(".all__tchk .tchk");
+
 let count = 0;
 let width;
 function init() {
+
   width = document.querySelector(".slider").offsetWidth;
+    console.log(img.length);
   sliderLine.style.width = width * img.length + "px";
   img.forEach((item) => {
     item.style.width = width + "px";
@@ -17,34 +20,32 @@ init();
 document.querySelector(".button-next").addEventListener("click", function () {
   count++;
 
-
-  console.log(img[1]);
   if (count >= img.length) {
     count = 0;
   }
+
   stepSlider();
+  // dot(count);
 });
 
 document
   .querySelector(".button-previous")
   .addEventListener("click", function () {
     count--;
-    console.log(count);
+
     if (count < 0) {
       count = img.length - 1;
     }
-    stepSlider();
-  });
 
+    stepSlider();
+    // dot(count);
+  });
 
 function stepSlider() {
   sliderLine.style.transform = `translate(-${count * width}px)`;
 }
 
-
-
-function dot(){
-
-      console.log(tchk);
-   
+function dot(index) {
+  dots.forEach((item) => item.classList.remove("tchk-activ"));
+  dots[index].classList.add("tchk-activ");
 }
