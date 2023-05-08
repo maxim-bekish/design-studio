b3e2f159ce6d416953961ced6fec0f58a78b74db
Maksim Bekis <maxamax997@gmail.com>
2023-05-09 01:18:50 +0300

    ready slider desktop and mobi

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let img = document.querySelectorAll(".sliderLine img");
-let sliderLine = document.querySelector(".sliderLine-mobi");
+let sliderLine = document.querySelector(".sliderLine");
 let dots = document.querySelectorAll(".all__tchk .tchk");
-
+let title_a = document.querySelectorAll(".section__row__up li");
 let count = 0;
 let width;
-function init() {
 
+function init() {
   width = document.querySelector(".slider").offsetWidth;
-    console.log(img.length);
   sliderLine.style.width = width * img.length + "px";
   img.forEach((item) => {
     item.style.width = width + "px";
@@ -19,33 +18,47 @@ window.addEventListener("resize", init);
 init();
 document.querySelector(".button-next").addEventListener("click", function () {
   count++;
-
   if (count >= img.length) {
     count = 0;
   }
-
   stepSlider();
-  // dot(count);
+  dot(count);
 });
 
 document
   .querySelector(".button-previous")
   .addEventListener("click", function () {
     count--;
-
     if (count < 0) {
       count = img.length - 1;
     }
-
     stepSlider();
-    // dot(count);
+    dot(count);
   });
 
 function stepSlider() {
   sliderLine.style.transform = `translate(-${count * width}px)`;
 }
 
+dots.forEach((dot_all, index) => {
+  dot_all.addEventListener("click", () => {
+    count = index;
+    stepSlider();
+    dot(count);
+  });
+});
+
+title_a.forEach((dot_all, index) => {
+  dot_all.addEventListener("click", () => {
+    count = index;
+    stepSlider();
+    dot(count);
+  });
+});
+
 function dot(index) {
   dots.forEach((item) => item.classList.remove("tchk-activ"));
   dots[index].classList.add("tchk-activ");
+  title_a.forEach((item) => item.classList.remove("a-activ"));
+  title_a[index].classList.add("a-activ");
 }
