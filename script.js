b8a9a586d8aab07637c400e267d8da9b020df8c9
Maksim Bekis <maxamax997@gmail.com>
2023-05-09 03:42:58 +0300

    rady slider +infa

--- script.js
+++ script.js
@@ -2,8 +2,58 @@ let img = document.querySelectorAll(".sliderLine img");
 let sliderLine = document.querySelector(".sliderLine");
 let dots = document.querySelectorAll(".all__tchk .tchk");
 let title_a = document.querySelectorAll(".section__row__up li");
+let grid_info = document.querySelectorAll(".grid__info");
+
 let count = 0;
 let width;
+let baseData = {
+  0: {
+    city: "Rostov-on-Don <br> LCD admiral",
+    apartment_area: "81 m2",
+    repair_time: "3.5 months",
+    repair_cost: "Upon request",
+  },
+  1: {
+    city: "Sochi <br> Thieves",
+    apartment_area: "105 m2",
+    repair_time: "4 months",
+    repair_cost: "Upon request",
+  },
+  2: {
+    city: "Rostov-on-Don <br> Patriotic",
+    apartment_area: "93 m2",
+    repair_time: "3 months",
+    repair_cost: "Upon request",
+  },
+};
+
+// ---------------------------------
+(function add_span() {
+  let nameClass = 1;
+  grid_info.forEach((item) => {
+    let span = document.createElement("span");
+    item.append(span);
+    span.className = `class${nameClass++}`;
+  });
+})();
+
+let span1 = document.querySelector(".class1");
+span1.innerHTML = baseData[0].city;
+let span2 = document.querySelector(".class2");
+span2.innerHTML = baseData[0].apartment_area;
+let span3 = document.querySelector(".class3");
+span3.innerHTML = baseData[0].repair_time;
+let span4 = document.querySelector(".class4");
+span4.innerHTML = baseData[0].repair_cost;
+
+function add_text(index) {
+  span1.innerHTML = baseData[index].city;
+  span2.innerHTML = baseData[index].apartment_area;
+  span3.innerHTML = baseData[index].repair_time;
+  span4.innerHTML = baseData[index].repair_cost;
+}
+
+// ---------------------
 
 function init() {
   width = document.querySelector(".slider").offsetWidth;
@@ -14,15 +64,35 @@ function init() {
   });
   stepSlider();
 }
+
+function dot(index) {
+  dots.forEach((item) => item.classList.remove("tchk-activ"));
+  dots[index].classList.add("tchk-activ");
+  title_a.forEach((item) => item.classList.remove("a-activ"));
+  title_a[index].classList.add("a-activ");
+}
+
+function stepSlider() {
+  sliderLine.style.transform = `translate(-${count * width}px)`;
+}
+
+function connect() {
+  stepSlider();
+  dot(count);
+  add_text(count);
+}
+
+// ------------------Event_beginning-----------------
+
 window.addEventListener("resize", init);
 init();
+
 document.querySelector(".button-next").addEventListener("click", function () {
   count++;
   if (count >= img.length) {
     count = 0;
   }
-  stepSlider();
-  dot(count);
+  connect();
 });
 
 document
@@ -32,33 +102,21 @@ document
     if (count < 0) {
       count = img.length - 1;
     }
-    stepSlider();
-    dot(count);
+    connect();
   });
-
-function stepSlider() {
-  sliderLine.style.transform = `translate(-${count * width}px)`;
-}
 
 dots.forEach((dot_all, index) => {
   dot_all.addEventListener("click", () => {
     count = index;
-    stepSlider();
-    dot(count);
+    connect();
   });
 });
 
 title_a.forEach((dot_all, index) => {
   dot_all.addEventListener("click", () => {
     count = index;
-    stepSlider();
-    dot(count);
+    connect();
   });
 });
 
-function dot(index) {
-  dots.forEach((item) => item.classList.remove("tchk-activ"));
-  dots[index].classList.add("tchk-activ");
-  title_a.forEach((item) => item.classList.remove("a-activ"));
-  title_a[index].classList.add("a-activ");
-}
+// ------------------Event_end-----------------
